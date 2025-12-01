3ee53fbdfb6a5788564432965810654b8665ca41
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2025-12-01 15:00:29 +0000

    Add Script for BlueTui opening

---
+++ scripts/openBluetui.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# If you have i3 window tiling manager, you can bind it on you .config/i3/config file by adding the line below:
+# bindsym <your keystroke> exec /path/to/openBluetui.sh
+
+TERMINAL="xterm"
+APP_COMMAND="bluetui"
+TARGET_WORKSPACE="4"
+
+i3-msg "exec $TERMINAL -T 'bluetui' -e $APP_COMMAND; \
+        workspace $TARGET_WORKSPACE; \
+        layout tabbed"
+
+echo "$APP_COMMAND launched in a new $TERMINAL window, moved to workspace $TARGET_WORKSPACE, and layout set to tabbed."
